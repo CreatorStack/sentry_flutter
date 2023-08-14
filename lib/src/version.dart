31c0396fa53ecbd7197ac01e1c38bdb312afb789
himanshugarg08 <himanshu.dma@gmail.com>
2023-08-14 16:49:14 +0530

    Added project

---
+++ lib/src/version.dart
@@ -0,0 +1,5 @@
+/// The SDK version reported to Sentry.io in the submitted events.
+const String sdkVersion = '6.18.3';
+
+/// The default SDK name reported to Sentry.io in the submitted events.
+const String sdkName = 'sentry.dart.flutter';
